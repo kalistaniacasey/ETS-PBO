d8652ab7ed46f67db70d0f4c66af5cc29614c34f
kalistaniacasey <162282241+kalistaniacasey@users.noreply.github.com>
2024-10-24 14:29:12 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+, "5025231302", "Informatika");
+
+        // Memanggil metode dari kelas induk
+        mahasiswa.perkenalanDiri();
+
+        // Memanggil metode dari kelas turunan
+        mahasiswa.tampilkanInfoMahasiswa();
+    }
+}
